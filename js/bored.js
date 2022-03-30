0d0069f264cd72e9e26b75213896c5d76020579c
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2022-03-30 11:27:50 +0200

    move king

--- js/bored.js
+++ js/bored.js
@@ -1,21 +1,27 @@
 "use strict";
 
 const boardElement = document.getElementById('board');
+
 const ticTacToeArray = [
     ["paintWhite", "paintBlack", "paintWhite"],
     ["paintBlack", "paintWhite", "paintBlack"],
     ["paintWhite", "paintBlack", "paintWhite"],
 ];
 const cheesArray = [
-    ["paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack"],
     ["paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite"],
     ["paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack"],
     ["paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite"],
     ["paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack"],
-    ["paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite"],
+    ["paintBlack", "paintWhite", "paintBlack", "paintWhite kingIcon", "paintBlack", "paintWhite", "paintBlack", "paintWhite"],
     ["paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack"],
-    ["paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite"]
+    ["paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite"],
+    ["paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack", "paintWhite", "paintBlack"]
 ];
+
+const kingPlasment = {
+    posY: -1,
+    posX: -1
+}
 
 function setupBoard(boardBlockHeight, boardBlockWidth) {
     boardElement.innerHTML = "";//clear board
@@ -26,12 +32,18 @@ function setupBoard(boardBlockHeight, boardBlockWidth) {
 
     boardElement.style.width = boardWidth + "px";
     boardElement.style.height = boardHeight + "px";
+
+    //reset kingPlasment
+    kingPlasment.posY = -1;
+    kingPlasment.posX = -1;
 }
 
-function makeBlock(cssColorClass) {
+function makeBlock(cssColorClass, idValue) {
     let block = document.createElement('div');
-    block.classList.add('block');
-    block.classList.add(cssColorClass);
+    block.id = idValue;//block.setAttribute("id", idValue);  
+    //block.classList.add('block');
+    //block.classList.add(cssColorClass);
+    block.className = 'block ' + cssColorClass;
     boardElement.appendChild(block);
 }
 
@@ -44,7 +56,7 @@ function makeTicTacToeBoard() {
         for (let xPos = 0; xPos < 3; xPos++) {
 
             const colorCss = ticTacToeArray[yPos][xPos];
-            makeBlock(colorCss);
+            makeBlock(colorCss, "Y" + yPos + "X" + xPos);
 
         }
     }
@@ -59,10 +71,35 @@ function makeCheesBoard() {
         for (let xPos = 0; xPos < 8; xPos++) {
 
             const colorCss = cheesArray[yPos][xPos];
-            makeBlock(colorCss);
+
+            if (colorCss.includes('kingIcon')) {
+                kingPlasment.posY = yPos;
+                kingPlasment.posX = xPos;
+            }
+
+            makeBlock(colorCss, "Y" + yPos + "X" + xPos);
 
         }
     }
+}
+
+function moveKingUp() {
+    if (kingPlasment.posY === -1) {//if there is no king placed
+        return;
+    }
+
+    const kingDiv = document.getElementById("Y" + kingPlasment.posY + "X" + kingPlasment.posX);
+    const kingsNewPosY = kingPlasment.posY - 1;
+
+    if (kingsNewPosY < 0 || kingsNewPosY > 7) {
+        return;//prevent king from going outside board.
+    }
+    const kingMoveToDiv = document.getElementById("Y" + kingsNewPosY + "X" + kingPlasment.posX);
+
+    kingDiv.classList.toggle("kingIcon");
+    kingMoveToDiv.classList.toggle("kingIcon");
+
+    kingPlasment.posY = kingPlasment.posY - 1;
 }
 
 document.getElementById("btn-whiteBlock").addEventListener("click", () => makeBlock("paintWhite"));
